c92bbc39db6e9ef293d5d7dbcd0dc30f1e4aca29
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-15 18:31:20 +0530

    added schema for user

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,14 @@
 // Add config, create user models and schema here;
-import {model, Schema} from "mongoose";
-const userModel = new Model({
+// mongodb+srv://harsh7562:whoooisharsh@mycluster.pffdh.mongodb.net/
+
+import {Model, Schema} from "mongoose";
+
+const UserSchema = new Schema({
     username: {
         type: String,
         unique: true
-    }
-})
+    },
+    Password: String
+});
+
+const userSchema = new Model(UserSchema, "User");
