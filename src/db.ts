d41584263cbe383d0a2341ab130f93029af1996d
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-16 01:52:56 +0530

    'code'

--- src/db.ts
+++ src/db.ts
@@ -12,4 +12,12 @@ const UserSchema = new Schema({
     password: String
 });
 
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true}
+});
+
 export const UserModel = model("User", UserSchema);
+export const ContentModel = model("Content", ContentSchema);
