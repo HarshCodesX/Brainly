dde86f74776eaf3912198fc37cb3c3532377cd36
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-15 18:25:06 +0530

    started initializing db models

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,8 @@
 // Add config, create user models and schema here;
+import {model, Schema} from "mongoose";
+const userModel = new Model({
+    username: {
+        type: String,
+        unique: true
+    }
+})
