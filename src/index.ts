c88936863746bdcf196fe9d26cb4b6c44941d394
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-16 16:12:11 +0530

    'code'

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ app.post("/api/v1/content", userMiddleware, async (req, res) => {
     const link = req.body.link;
     const type = req.body.type;
     await ContentModel.create({
-        title
+        title,
         link, 
         type,
         //@ts-ignore
