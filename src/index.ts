35a81c45d07b49d0626af3a2a4aa990dba2ad91e
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-16 16:12:45 +0530

    'code'

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ app.post("/api/v1/signin", async (req, res) => {
 });
 
 app.post("/api/v1/content", userMiddleware, async (req, res) => {
-    const title = req.bpdy.title;
+    const title = req.body.title;
     const link = req.body.link;
     const type = req.body.type;
     await ContentModel.create({
